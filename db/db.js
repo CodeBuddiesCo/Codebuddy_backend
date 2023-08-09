c188e664df1d2af3e7945036a6612416f9319f78
Hollye Edge <hollyekedge@gmail.com>
2023-08-08 20:26:37 -0600

    changing main db.js to connection pool

--- db/db.js
+++ db/db.js
@@ -1,16 +1,21 @@
-const mysql = require('mysql');
+const mysql2 = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+console.log(process.env.DB_HOST)
+
+const db = mysql2.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the database');
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 50, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
 module.exports = db;
+
