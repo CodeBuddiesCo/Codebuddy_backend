71b115f809dbdbcabd0d10ff72157779ee87b20a
Hollye Edge <hollyekedge@gmail.com>
2023-08-15 12:28:36 -0600

    changes to users for mysql2

--- api/users.js
+++ api/users.js
@@ -3,47 +3,62 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const db = require('../db/db');
 const { requireUser, requireAdmin } = require('./utils');
+const { getUserbyUserNameOrEmail, createUser, getUserbyUserName } = require('../db/users');
+
 
 const usersRouter = express.Router();
 
-usersRouter.post('/register', (req, res) => {
+usersRouter.post('/register', async (req, res) => {
   const { name, email, username, password } = req.body;
   const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error while registering user');
+  try {
+
+    if (password.length < 6) {
+      console.error("Provided Password does not meet requirements");
+      return res.status(400).send('Password is too short');
+    }
+    
+    const userExists = await getUserbyUserNameOrEmail(username, email);
+    console.log("UserExists function result ->", userExists)
+
+    if (userExists !== "Username or email ok to use") {
+      console.error(`${username} or ${email} is already in use.`);
+      return res.status(400).send(`${username} or ${email} is already in use.`);
     }
 
-    const sql = 'INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)';
-    db.query(sql, [name, email, username, hashedPassword], (err, result) => {
-      if (err) {
-        if (err.code === 'ER_DUP_ENTRY') {
-          return res.status(409).send('Email or username already exists');
-        }
-        throw err;
-      }
-
-      const userId = result.insertId;
-
-      const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-      });
-
-      res.json({
-        message: 'User registered successfully',
-        token,
-      });
+    const [newUser] = await createUser({
+      name,
+      email,
+      username,
+      hashedPassword,
     });
-  });
+    console.log(newUser)
+
+    const token = jwt.sign(newUser, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    console.log("New user token ->", token)
+    res.send({
+      message: "Thank you for registering.",
+      token,
+      user: newUser,
+    });
+    
+  } catch (error) {
+      console.error(error);
+      return res.status(500).send('Error while registering user');
+  }
 });
 
-usersRouter.post('/login', function (req, res) {
+usersRouter.post('/login', async function (req, res) {
   const { username, password } = req.body;
 
   console.log('Login request received for username:', username);
 
+  try {
+    
   if (!username) {
     console.log('No username provided');
     return res.status(400).send('Username is required');
@@ -54,15 +69,10 @@ usersRouter.post('/login', function (req, res) {
     return res.status(400).send('Password is required');
   }
 
-  const sql = 'SELECT * FROM users WHERE username = ?';
-  db.query(sql, [username], function (err, results) {
-    if (err) {
-      console.log('Database query error:', err);
-      return res.status(500).send('Internal Server Error');
-    }
+  const [user] = await getUserbyUserName(username);
+  console.log("User ->", user)
 
-    if (results.length > 0) {
-      const user = results[0];
+    if (user.username) {
       console.log('User found:', user.username);
 
       bcrypt.compare(password, user.password, function(err, result) {
@@ -70,21 +80,30 @@ usersRouter.post('/login', function (req, res) {
           console.log('bcrypt compare error:', err);
           return res.status(500).send('Internal Server Error');
         }
-
+        delete user.password
         if (result) {
           const token = jwt.sign({ id: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
           console.log('Login successful, token:', token);
-          return res.json({ token });
+          res.send({
+            message: "Login successful",
+            token,
+            user: user,
+          });
         } else {
           console.log('Login failed: Invalid credentials');
           return res.status(401).send('Invalid credentials');
         }
       });
+      
     } else {
       console.log('Login failed: No user found');
       return res.status(401).send('Invalid credentials');
     }
-  });
+  
+} catch (error) {
+  console.log('Database query error:', error);
+  return res.status(500).send('Internal Server Error');
+}
 });
 
 
