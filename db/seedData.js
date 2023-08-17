b51686ffac0324178debe02691e1970a8f308bba
cvmug <catherine.mugnai@gmail.com>
2023-08-17 14:05:59 -0400

    redid messages

--- db/seedData.js
+++ db/seedData.js
@@ -85,7 +85,7 @@ const createTables = async () => {
       id INT AUTO_INCREMENT PRIMARY KEY,
       sender_id INT NOT NULL,
       receiver_id INT NOT NULL,
-      content TEXT NOT NULL,
+      message_content TEXT NOT NULL,
       timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (sender_id) REFERENCES users(id),
       FOREIGN KEY (receiver_id) REFERENCES users(id)
