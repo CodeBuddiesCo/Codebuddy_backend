f2aa8f5fbd8d5778174fd1ee99f0c6bc0415170c
cvmug <113868111+cvmug@users.noreply.github.com>
2025-09-24 22:50:22 -0400

    updated util transport

--- api/utils.js
+++ api/utils.js
@@ -52,7 +52,9 @@ function validateToken(req, res, next) {
 }
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
