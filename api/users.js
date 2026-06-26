a232edf5d3dbeb0cda33d74046d034b6d4cfa18a
cvmug <113868111+cvmug@users.noreply.github.com>
2026-06-25 22:42:23 -0400

    removed security questions

--- api/users.js
+++ api/users.js
@@ -22,7 +22,6 @@ const {
   getSecurityQuestions,
   verifySecurityAnswers,
   resetPassword,
-  updateSecurityQuestionsAndAnswers,
   demoteUserFromBuddy,
   getUsersFollowedByUser,
   followUser,
@@ -412,31 +411,6 @@ usersRouter.post('/reset-password', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error resetting password' });
-  }
-});
-
-// Update security questions and answers
-usersRouter.put('/update-security/:id', async (req, res) => {
-  const userId = req.params.id;
-  const { security_question_1, security_answer_1, security_question_2, security_answer_2, security_question_3, security_answer_3 } = req.body;
-
-  try {
-    // Ensure all required fields are provided
-    if (!security_question_1 || !security_answer_1 || !security_question_2 || !security_answer_2 || !security_question_3 || !security_answer_3) {
-      return res.status(400).json({ error: 'All security questions and answers are required' });
-    }
-
-    // Update security questions and answers in the database
-    const wasUpdated = await updateSecurityQuestionsAndAnswers(userId, security_question_1, security_answer_1, security_question_2, security_answer_2, security_question_3, security_answer_3);
-
-    if (wasUpdated) {
-      res.status(200).json({ message: 'Security questions and answers updated successfully' });
-    } else {
-      res.status(404).json({ error: 'User not found' });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error updating security questions and answers' });
   }
 });
 
